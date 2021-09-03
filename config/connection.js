156df1a072ba2a452f0b2b810e5103d29137be22
lucawic <lucaswick@email.arizona.edu>
2021-09-03 11:26:34 -0700

    One of many

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 
 //create connection to the database, pass in your mySQL information for username and password
-const sequelize = new Sequelize ('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize (process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
